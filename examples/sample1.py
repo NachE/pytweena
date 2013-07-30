7ae4f4ab2e011a83533a2fd9cd6ad32248bff5af
NachE <nache.nache@gmail.com>
2013-07-30 13:55:05 +0200

    updating sample1 file

--- examples/sample1.py
+++ examples/sample1.py
@@ -15,10 +15,34 @@ myapi = pytweena.api
 #Auth the api
 myapi.auth(consumerkey, consumersecret, accesstoken, accesstokensecret)
 
-#Get latest 10 twits from timeline
+
+########################################
+print ("\n\n\nGet latest 10 tweets from timeline")
 myapi.home_timeline({'count': 10})
 for tweet in myapi.jsondata:
-    print tweet['text']
+	print tweet['text']
+########################################
 
-#Send a tweet
+
+
+########################################
+print ("\n\n\nGet tweet by id")
+myapi.show({'id':8062317551})
+print myapi.jsondata['text']
+#######################################
+
+
+
+##########################################
+print ("\n\n\nSearch 'my search string', show only 10")
+myapi.search_tweets({'q': 'my search string','count': 10})
+for tweet in myapi.jsondata['statuses']:
+	print tweet['text']
+##########################################
+
+
+
+###########################################
+print("\n\n\nSend a tweet")
 myapi.update({'status': 'Hi!, im an app with pytweena lib!'})
+###########################################
