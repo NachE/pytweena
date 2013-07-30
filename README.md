61bc745de5c551c4cf03b29995e31e0258a8a2fb
NachE <nache.nache@gmail.com>
2013-07-30 10:46:54 +0200

    Update README file and sample1.py

--- README.md
+++ README.md
@@ -13,4 +13,20 @@ Roadmap
 
 Status
 ======
+ * REST API v1.1:
 Timelines support: OK
+Tweets support: OK except media upload
+Search support: TODO
+Streaming support: TODO
+Direct Messages support: TODO
+Friends & Followers support: TODO
+Users support: TODO
+Suggested Users support: TODO
+Favorites support: TODO
+Lists support: TODO
+Saved searches support: TODO
+Places & Geo support: TODO
+Trends support: TODO
+Spam Reporting support: TODO
+OAuth support: OK with python-oauth2 lib
+Help support: TODO 
--- examples/sample1.py
+++ examples/sample1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Import the pytweena lib
+#Import pytweena lib
 import pytweena
 
 #Set the keys and tokens
@@ -12,12 +12,13 @@ accesstokensecret = ""
 #Get the api
 myapi = pytweena.api
 
-#Auth
+#Auth the api
 myapi.auth(consumerkey, consumersecret, accesstoken, accesstokensecret)
 
-#Run home_timeline
-myapi.home_timeline()
-
-#foreach json response
+#Get latest 10 twits from timeline
+myapi.home_timeline({'count': 10})
 for tweet in myapi.jsondata:
     print tweet['text']
+
+#Send a tweet
+myapi.update({'status': 'Hi!, im an app with pytweena lib!'})
